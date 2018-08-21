a6ddff025467f86c50411dbce76a727de5d3dd3d
Zaur Nasibov <zaur@znasibov.info>
2018-08-22 00:18:28 +0300

    Start setting merger

--- src/concrete_settings.py
+++ src/concrete_settings.py
@@ -1,7 +1,13 @@
+# IDEAS:
+# * Exceptions with 'hint', 'colour_message', 'message' fields
+#   * common messages about OverrideSettings
+#
+
 import copy
 import re
 import sys
-from typing import Any, Callable, List, Tuple, get_type_hints
+import types
+from typing import Any, Callable, Sequence, Union
 
 PY_VERSION = (sys.version_info.major, sys.version_info.minor)
 PY_35 = (3, 5)
@@ -9,36 +15,43 @@ PY_35 = (3, 5)
 if PY_VERSION < PY_35:
     raise ImportError('Python < 3.5 is not supported')
 
+ValidatorsOrNone = Union[Callable, Sequence[Callable], None]
+
 
 class Undefined:
     pass
 
 
-def validate_type(settings, new_val):
+class GuessSettingType:
+    """A special value for Setting.type_hint, which indicates
+       that a Setting type should be derived from the default value."""
+    pass
+
+
+def validate_type(setting, val):
     pass
 
 
 class Setting:
     # TODO: slots
 
-    override: bool
     val: Any
-
+    _type_hint: Any
 
     def __init__(self,
                  default_val: Any = Undefined,
                  description: str = '',
-                 type_hint: Any = Any,
+                 validators: ValidatorsOrNone = (validate_type, ),
+                 type_hint: Any = GuessSettingType):
 
-                 validator: Callable = validate_type,
-                 sealed: bool = False,
-                 override: bool = False):
-        self.val = default_val
-        self.type_hint = type_hint
+        self.value = default_val
         self.description = description
-        self.validator = validator
-        self.sealed = sealed
-        self.override = override
+        self.type_hint = type_hint
+
+        if isinstance(validators, types.FunctionType):
+            self.validators = (validators, )
+        else:
+            self.validators = validators or ()
 
         self.name = ''
         self.owner_name = ''
@@ -51,117 +64,180 @@ class Setting:
     def full_name(self):
         return self.owner_name + '_' + self.name
 
+    @property
+    def type_hint(self):
+        return self._type_hint
+
+    @type_hint.setter
+    def type_hint(self, val):
+        if val == GuessSettingType:
+            self._type_hint = guess_type_hint(self.value)
+        else:
+            self._type_hint = val
+
     def __get__(self, obj, objtype):
-        if self.val == Undefined:
+        # TODO: VALIDATOR
+        if self.value == Undefined:
             raise ValueError(f'Setting {self.full_name} value has not been set')
 
         if obj:
             # object.field access
-            if callable(self.val):
-                return self.val(obj)
+            if callable(self.value):
+                return self.value(obj)
 
-        return self.val
+        return self.value
 
     def __set__(self, obj, val):
-        if self.sealed:
-            raise AttributeError(f'Cannot set sealed setting {self.full_name} value')
+        for validator in self.validators:
+            validator(self, val)
+        self.value = val
 
-        if self.validator:
-            self.validator(self, val)
-        self.val = val
+
+class SealedSetting(Setting):
+    pass
 
 
 class OverrideSetting(Setting):
-    def __init__(self, *args, **kwargs):
-        kwargs['override'] = True
-        super().__init__(self, *args, **kwargs)
+    pass
 
 
 
 class SettingsMeta(type):
-    def __new__(cls, name, bases, dct):
+    def __new__(cls, name, bases, class_dict):
+        base_dict = {}
         for base in bases:
             if not issubclass(base, Settings):
                 raise TypeError('Settings class can inherit from from other Settings classes only')
+            # TODO:
+            if base is not Settings:
+                base_dict = base.__dict__
 
-        new_dct = cls.class_dict_to_settings(dct)
+        new_dict = cls.class_dict_to_settings(class_dict)
+
         if PY_VERSION == PY_35:
-            cls._py35_set_name(cls, new_dct)
+            cls._py35_set_name(cls, new_dict)
 
-
-        cls = super().__new__(cls, name, bases, new_dct)
+        new_dict = cls.merge_into_class_dict(new_dict, base_dict)
+        cls = super().__new__(cls, name, bases, new_dict)
         return cls
 
     @classmethod
-    def _py35_set_name(self, cls, dct):
+    def _py35_set_name(self, cls, class_dict):
         '''__set_name__() implementation for Python 3.5'''
-        for name, field in dct.items():
+        for name, field in class_dict.items():
             if isinstance(field, Setting):
                 field.__set_name__(cls, name)
 
     @classmethod
-    def class_dict_to_settings(cls, dct):
-        new_dct = {}
-        annotations = dct.get('__annotations__', {})
+    def class_dict_to_settings(cls, class_dict):
+        new_dict = {}
+        annotations = class_dict.get('__annotations__', {})
 
-        for attr, val in dct.items():
+        for attr, val in class_dict.items():
             if isinstance(val, Setting):
-                new_dct[attr] = val
+                new_dict[attr] = val
             elif cls.is_setting_name(attr):
-                new_dct[attr] = cls.make_setting_from_attr(attr, val, annotations)
+                new_dict[attr] = cls.make_setting_from_attr(attr, val, annotations)
             else:
-                new_dct[attr] = val
-        return new_dct
+                new_dict[attr] = val
+        return new_dict
 
     @staticmethod
     def make_setting_from_attr(attr, val, annotations):
-        class Guess:
-            pass
-
         if callable(val):
             type_hint = val.__annotations__.get('return', Any)
         else:
-            type_hint = annotations.get(attr, Guess)
+            type_hint = annotations.get(attr, GuessSettingType)
+
         return Setting(val, type_hint=type_hint)
-
-    @classmethod
-    def merge_bases_settings(cls, bases):
-        '''
-        Iterate through class bases, lookup `Setting` descriptor-fields
-        '''
-        setting_fields = {}
-
-        # Python resolves base attributes from right to left, i.e. leftmost has precedence.
-        # Thus we should iterate in reverse to detect the changed fields:
-        for base in reversed(bases):
-            for name, base_field in base.__dict__.items():
-                # filter Settings
-                if not isinstance(base_field, Setting):
-                    continue
-
-                # In case of multiple base classes with same settings
-                # - Mixins or multiple inheritance,
-                # check if setting has already been defined
-                # and update its history if required
-                if name in setting_fields:
-                    old_field = setting_fields[name]
-                    old_field.update(base, base_field)
-                else:
-                    setting_fields[name] = base_field
-
-        return setting_fields
-
-    def update_setting(cls, old_setting, new_setting):
-        if not isinstance(new_setting, OverrideSetting):
-            raise AttributeError(
-                f'Setting {self.name} defined in {self.owner_name} is overriden. '
-                'Use OverrideSetting class to override a setting definition explicitly.'
-            )
 
     @staticmethod
     def is_setting_name(name: str) -> bool:
         '''Return true if name is written in upper case'''
         return name.upper() == name
+
+    @classmethod
+    def merge_into_class_dict(cls, class_dict, bases_dict):
+        new_dict = {}
+
+        for attr, field in class_dict.items():
+            if attr not in bases_dict:
+                # no extra processing for attributes which
+                # are not among bases attributes
+                new_dict[attr] = field
+                continue
+
+            base_field = bases_dict[attr]
+            if not isinstance(field, Setting) and not isinstance(base_field, Setting):
+                new_dict[attr] = field
+                continue
+
+            new_dict[attr] = cls._merge_fields(attr, field, base_field)
+
+        return new_dict
+
+    @staticmethod
+    def _merge_fields(attr, field, base_field):
+        # class field is defined as OverrideSetting
+        if isinstance(field, OverrideSetting):
+            if isinstance(base_field, Setting):
+                # TODO: record in history
+                pass
+            else:
+                # TODO: WARNING record in history
+                pass
+
+        # class field is defined as Setting, i.e. no override precedence.
+        elif isinstance(field, Setting):
+            if isinstance(base_field, SealedSetting):
+                raise AttributeError(
+                    f'TODO: History; Setting "{attr}" in class "[TODO]"'
+                    ' is sealed and cannot be changed in class "[TODO]".'
+                    ' Define a setting via OverrideSetting() descriptor to override'
+                    ' the sealed setting type and value explicitly.'
+                )
+            elif isinstance(base_field, Setting):
+                if field.type_hint == GuessSettingType:
+                    # TODO: record in history
+                    field.type_hint = base_field.type_hint
+                elif field.type_hint != base_field.type_hint:
+                    raise AttributeError(
+                        f'TODO: History; Setting "{attr}" in class "[TODO]"'
+                        ' has a different type hint that definition in class "[TODO]".'
+                        ' Define a setting via OverrideSetting() descriptor to override'
+                        ' the existing setting type explicitly.'
+                    )
+
+                if field.description and field.description != base_field.description:
+                    raise AttributeError(
+                        f'TODO: History; Setting "{attr}" in class "[TODO]"'
+                        ' has a different description than definition in class "[TODO]".'
+                        ' Define a setting via OverrideSetting() descriptor to override'
+                        ' the existing setting description explicitly.'
+                    )
+                else:
+                    field.description = base_field.description
+
+                field.validators = base_field.validators
+                raise Exception('TODO: WRITE A UNIT TEST')
+
+
+            else:
+                raise AttributeError(
+                    f'TODO: History; Setting in class "[TODO]" overrides an'
+                    ' existing attribute "{attr}" defined in class "[TODO]".'
+                    ' Define a setting via OverrideSetting() descriptor to override'
+                    ' the attribute explicitly.'
+                )
+        return field #, diff # <- TODO History
+
+
+def guess_type_hint(val):
+    # TODO: unit tests for this function
+    if isinstance(val, int):
+        return int
+    elif isinstance(val, float):
+        return float
 
 
 class Settings(metaclass=SettingsMeta):
--- src/test_concrete_settings.py
+++ src/test_concrete_settings.py
@@ -1,19 +1,22 @@
 import random
+import types
 
 import factory
 import pytest
 from factory.fuzzy import FuzzyInteger, FuzzyFloat, FuzzyText
 
-from concrete_settings import Settings, Setting, OverrideSetting
+from concrete_settings import Settings, Setting, SealedSetting, OverrideSetting
 
 seed = random.randint(1, 1e9)
 print(f'Running tests with seed: {seed:0>10}')
 factory.fuzzy.reseed_random(seed)
 
-# TODO use factory_boy
+
 INT_VAL: int = FuzzyInteger(-10e10, 10e10).fuzz()
 FLOAT_VAL: float = FuzzyFloat(-10e10, 10e10).fuzz()
-STR_VAL: str = FuzzyText(FuzzyInteger(1, 255)).fuzz()
+STR_VAL: str = FuzzyText(length=FuzzyInteger(1, 255).fuzz()).fuzz()
+
+STR_CONST: str = 'HELLO DESCRIPTION'
 
 
 def test_value_attribute_converted_to_setting():
@@ -25,6 +28,15 @@ def test_value_attribute_converted_to_setting():
     assert DEMO.type_hint is int
     assert S0.DEMO == INT_VAL
     assert S0().DEMO == INT_VAL
+
+
+def test_value_attribute_not_converted_to_setting():
+    class S0(Settings):
+        demo: int = INT_VAL
+
+    assert isinstance(S0.demo, int)
+    assert S0().demo == INT_VAL
+
 
 def test_method_converted_to_setting():
     class S0(Settings):
@@ -39,7 +51,18 @@ def test_method_converted_to_setting():
     assert S0().DEMO == INT_VAL
 
 
-def test_value_override():
+def test_method_not_converted_to_setting():
+    class S0(Settings):
+        x = INT_VAL
+
+        def demo(self) -> int:
+            return self.x
+
+    assert isinstance(S0.demo, types.FunctionType)
+    assert S0().demo() == INT_VAL
+
+
+def test_value_change_in_derived_settings():
     class S0(Settings):
         DEMO: int = INT_VAL + 1
 
@@ -48,8 +71,64 @@ def test_value_override():
 
     DEMO = S1.__dict__['DEMO']
     assert isinstance(DEMO, Setting)
-    #assert DEMO.type_hint is int
-    #assert S1().DEMO == INT_VAL
+    assert S1().DEMO == INT_VAL
+
+
+def test_setting_attributes_copied_in_derived_settings():
+    def dummy_validator(setting, val):
+        pass
+
+    class S0(Settings):
+        DEMO: int = Setting(INT_VAL, STR_CONST, dummy_validator)
+
+    class S1(S0):
+        DEMO = INT_VAL + 1
+
+    DEMO = S1.__dict__['DEMO']
+    assert DEMO.description == STR_CONST
+    assert DEMO.validators == (dummy_validator, )
+    assert DEMO.type_hint is int
+    assert S1().DEMO == INT_VAL + 1
+
+
+def test_description_change_in_derived_settings_error():
+    class S0(Settings):
+        DEMO: int = Setting(INT_VAL, STR_CONST)
+
+    with pytest.raises(AttributeError) as e:
+        class S1(S0):
+            DEMO: int = Setting(INT_VAL, STR_CONST + 'a')
+    e.match('has a different description')
+
+
+def test_sealed_setting_change_error():
+    class S0(Settings):
+        DEMO: int = SealedSetting(INT_VAL)
+
+    with pytest.raises(AttributeError) as e:
+        class S1(S0):
+            DEMO = INT_VAL + 1
+    e.match('is sealed and cannot be changed')
+
+
+def test_setting_change_type_in_derived_settings_error():
+    class S0(Settings):
+        DEMO: int = INT_VAL
+
+    with pytest.raises(AttributeError) as e:
+        class S1(S0):
+            DEMO = Setting(FLOAT_VAL)
+    e.match('has a different type hint')
+
+
+def test_attribute_change_in_derived_settings_error():
+    class S0(Settings):
+        demo: int = INT_VAL
+
+    with pytest.raises(AttributeError) as e:
+        class S1(S0):
+            demo = Setting(INT_VAL)
+    e.match('overrides an existing attribute')
 
 
 def test_non_settings_base_classes_not_allowed():
