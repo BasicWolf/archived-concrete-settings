1c4c4c3aeda1b5c099c322e41a9bcc73aad6251b
Zaur Nasibov <zaur@znasibov.info>
2019-08-18 20:59:28 +0300

    Add YAML Source tests

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -179,37 +179,48 @@ def test_yaml_source_read_int_value(fs):
     assert ysrc.read(S('A')) == 10
 
 
-# def test_json_source_read_float_value(fs):
-#     fs.create_file('/test/settings.json', contents='{"A": 10.25}')
-#     jsrc = get_source('/test/settings.json')
-#     jsrc.read(S('A')) == 10.25
+def test_yaml_source_read_float_value(fs):
+    fs.create_file('/test/settings.yaml', contents='A: 10.25')
+    ysrc = get_source('/test/settings.yaml')
+    assert ysrc.read(S('A')) == 10.25
 
 
-# def test_json_source_read_str_value(fs):
-#     fs.create_file('/test/settings.json', contents='{"A": "abc"}')
-#     jsrc = get_source('/test/settings.json')
-#     jsrc.read(S('A')) == "abc"
+def test_yaml_source_read_str_value(fs):
+    fs.create_file('/test/settings.yaml', contents='A: abc')
+    ysrc = get_source('/test/settings.yaml')
+    assert ysrc.read(S('A')) == "abc"
 
 
-# def test_json_source_read_null_value(fs):
-#     fs.create_file('/test/settings.json', contents='{"A": null}')
-#     jsrc = get_source('/test/settings.json')
-#     jsrc.read(S('A')) == None
+def test_yaml_source_read_null_value(fs):
+    fs.create_file('/test/settings.yaml', contents='A: null')
+    ysrc = get_source('/test/settings.yaml')
+    assert ysrc.read(S('A')) == None
 
 
-# def test_json_source_read_array_value(fs):
-#     fs.create_file('/test/settings.json', contents='{"A": [1, 2, 3]}')
-#     jsrc = get_source('/test/settings.json')
-#     jsrc.read(S('A')) == [1, 2, 3]
+def test_yaml_source_read_array_value(fs):
+    fs.create_file('/test/settings.yaml', contents='''
+    A:
+      - 1
+      - 2
+      - 3
+    ''')
+    ysrc = get_source('/test/settings.yaml')
+    assert ysrc.read(S('A')) == [1, 2, 3]
 
 
-# def test_json_source_read_object_value(fs):
-#     fs.create_file('/test/settings.json', contents='{"A": {"B": 10}}')
-#     jsrc = get_source('/test/settings.json')
-#     jsrc.read(S('A')) == {"B": 10}
+def test_yaml_source_read_object_value(fs):
+    fs.create_file('/test/settings.yaml', contents='''
+    A:
+      B: 10
+    ''')
+    ysrc = get_source('/test/settings.yaml')
+    assert ysrc.read(S('A')) == {'B': 10}
 
 
-# def test_json_source_read_nested_object_value(fs):
-#     fs.create_file('/test/settings.json', contents='{"A": {"B": 10}}')
-#     jsrc = get_source('/test/settings.json')
-#     jsrc.read(S('B'), parents=('A',)) == 10
+def test_yaml_source_read_nested_object_value(fs):
+    fs.create_file('/test/settings.yaml', contents='''
+    A:
+      B: 10
+    ''')
+    ysrc = get_source('/test/settings.yaml')
+    assert ysrc.read(S('B'), parents=('A',)) == 10
