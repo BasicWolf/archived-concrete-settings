ccad53984be8263b77939ad500f9aafb244c67aa
Zaur Nasibov <zaur@znasibov.info>
2020-01-26 21:07:29 +0200

    Make type_hints_equal Python 3.6 -compatible

--- concrete_settings/types.py
+++ concrete_settings/types.py
@@ -1,3 +1,5 @@
+import sys
+
 from typing import Any
 
 
@@ -52,9 +54,27 @@ class GuessSettingType:
 
 
 def type_hints_equal(hint1, hint2):
-    if hint1 == hint2:
+    """Returns true if type hints equal.
+
+    For example:
+
+        type_hints_equal(typing.List, list) => True
+    """
+    assert hint1 is not None
+    assert hint2 is not None
+
+    if hint1 is hint2:
         return True
 
-    h1 = getattr(hint1, '__origin__', hint1)
-    h2 = getattr(hint2, '__origin__', hint2)
-    return h1 == h2
+    h1 = getattr(hint1, '__origin__', hint1) or hint1  # `... or hint 1`
+    h2 = getattr(hint2, '__origin__', hint2) or hint2  # - protect from `None` value
+
+    if h1 is h2:
+        return True
+
+    PYTHON_36 = (3, 6)
+    if (sys.version_info.major, sys.version_info.minor) == PYTHON_36:
+        h1 = getattr(hint1, '__extra__', hint1)
+        h2 = getattr(hint2, '__extra__', hint2)
+
+    return h1 is h2
