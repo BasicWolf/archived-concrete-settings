a121c47c1611a01c7617fb6ff5120b4831773447
Zaur Nasibov <zaur@znasibov.info>
2020-01-28 20:22:44 +0200

    Restore accidentally deleted behaviors tests

---
+++ tests/test_behaviors/test_deprecated.py
@@ -0,0 +1,74 @@
+import pytest
+
+from concrete_settings import Settings, setting
+from concrete_settings.exceptions import SettingsValidationError
+from concrete_settings.contrib.behaviors import deprecated
+
+
+# == Deprecated == #
+
+
+def test_deprecated_warns_when_validating():
+    class S(Settings):
+        D = 10 @ deprecated
+
+    with pytest.warns(DeprecationWarning):
+        S().is_valid()
+
+
+def test_deprecated_warning_message():
+    class S(Settings):
+        D = 10 @ deprecated()
+
+    with pytest.warns(
+        DeprecationWarning,
+        match=r"Setting `D` in class `<class 'tests.test_behaviors.test_deprecated."
+        "test_deprecated_warning_message.<locals>.S'>` is deprecated.",
+    ) as w:
+        S().is_valid()
+        assert len(w) == 1
+
+
+def test_deprecated_error_when_validating():
+    class S(Settings):
+        D = 10 @ deprecated(error_on_validation=True)
+
+    with pytest.raises(SettingsValidationError):
+        S().is_valid(raise_exception=True)
+
+
+def test_deprecated_warns_on_get():
+    class S(Settings):
+        D = 10 @ deprecated(warn_on_get=True)
+
+    with pytest.warns(DeprecationWarning):
+        S().D
+
+
+def test_deprecated_warns_on_set():
+    class S(Settings):
+        D = 10 @ deprecated(warn_on_set=True)
+
+    with pytest.warns(DeprecationWarning):
+        S().D = 20
+
+
+def test_deprecated_not_warns():
+    class S(Settings):
+        D = 10 @ deprecated(warn_on_validation=False)
+
+    with pytest.warns(None):
+        S().is_valid()
+        S().D
+        S().D = 20
+
+
+def test_deprecated_on_property_setting():
+    class S(Settings):
+        @deprecated
+        @setting
+        def D(self):
+            return 30
+
+    with pytest.warns(DeprecationWarning):
+        assert S().is_valid()
---
+++ tests/test_behaviors/test_required.py
@@ -0,0 +1,24 @@
+import pytest
+
+from concrete_settings import Settings, Undefined
+from concrete_settings.exceptions import SettingsValidationError
+from concrete_settings.contrib.behaviors import required
+
+
+def test_required_behavior_validation_fails_when_value_is_undefined():
+    class MySettings(Settings):
+        DEBUG = Undefined @ required
+
+    with pytest.raises(
+        SettingsValidationError,
+        match="Setting `DEBUG` is required to have a value. "
+        "Current value is `Undefined`",
+    ):
+        MySettings().is_valid(raise_exception=True)
+
+
+def test_required_behavior_validation_ok_when_value_is_not_undefined():
+    class MySettings(Settings):
+        DEBUG = True @ required
+
+    assert MySettings().is_valid()
